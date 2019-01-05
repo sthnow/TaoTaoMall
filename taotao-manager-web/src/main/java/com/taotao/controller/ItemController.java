7b621ffbbf7512da88343ff20e7b29f2894c133b
wangzh <568780720@qq.com>
2019-01-05 23:44:39 +0800

    解决问题，排除dubbo依赖的spring可能导致的错误

--- taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
+++ taotao-manager-web/src/main/java/com/taotao/controller/ItemController.java
@@ -2,6 +2,8 @@ package com.taotao.controller;
 
 
 import com.taotao.pojo.TbItem;
+
+//import com.taotao.service.ItemService;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 /**
- * 商品管理controller
+ * 商品管理Controller
  */
 public class ItemController {
 
