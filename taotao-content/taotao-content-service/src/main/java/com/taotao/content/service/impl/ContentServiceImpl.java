474307575543d0cb5050df6e4d2669f7a83b2d67
wangzh <568780720@qq.com>
2019-01-20 22:14:15 +0800

    向业务逻辑中添加缓存

--- taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
+++ taotao-content/taotao-content-service/src/main/java/com/taotao/content/service/impl/ContentServiceImpl.java
@@ -1,12 +1,17 @@
 package com.taotao.content.service.impl;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.content.service.ContentService;
+import com.taotao.jedis.JedisClient;
 import com.taotao.mapper.TbContentMapper;
 import com.taotao.pojo.TbContent;
 import com.taotao.pojo.TbContentExample;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import redis.clients.jedis.Jedis;
 
 import java.util.Date;
 import java.util.List;
@@ -15,7 +20,11 @@ import java.util.List;
 public class ContentServiceImpl implements ContentService {
 
     @Autowired
-    TbContentMapper contentMapper;
+    private TbContentMapper contentMapper;
+    @Autowired
+    private JedisClient jedisClient;
+    @Value("${INDEX_CONTENT}")
+    private String INDEX_CONTENT;
     @Override
     public TaotaoResult addContent(TbContent content) {
        //补全pojo属性
@@ -59,6 +68,22 @@ public class ContentServiceImpl implements ContentService {
 
     @Override
     public List<TbContent> getContentByCid(long cid) {
+        //先查询缓存
+        //添加缓存不能影响正常的业务逻辑
+        try{
+            //查询缓存
+            //cid+""  可以转换成字符串？
+            String json = jedisClient.hget(INDEX_CONTENT, cid + "");
+            //查询到结果，把json转换成list返回
+            if(StringUtils.isNotBlank(json)){
+                List<TbContent> list = JsonUtils.jsonToList(json, TbContent.class);
+                return list;
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //缓存中没有命中，需要查询数据库
+
 
         TbContentExample example = new TbContentExample();
         TbContentExample.Criteria criteria = example.createCriteria();
@@ -66,6 +91,14 @@ public class ContentServiceImpl implements ContentService {
         //执行查询
         List<TbContent> list = contentMapper.selectByExample(example);
 
+        //把结果添加到缓存
+        try{
+            jedisClient.hset(INDEX_CONTENT, cid + "", JsonUtils.objectToJson(list));
+        }catch (Exception e){
+                e.printStackTrace();
+
+        }
+        //返回结果
         return list;
     }
 }
