92fe9857c520079624a0eaeeee74dee80fa4f974
wangzh <568780720@qq.com>
2019-01-29 12:23:56 +0800

    商品实现分析

---
+++ taotao-common/src/main/java/com/taotao/common/pojo/SearchResult.java
@@ -0,0 +1,25 @@
+package com.taotao.common.pojo;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SearchResult implements Serializable {
+    public int getTotlaPages() {
+        return totlaPages;
+    }
+
+    public void setTotlaPages(int totlaPages) {
+        this.totlaPages = totlaPages;
+    }
+
+    public List<SearchItem> getItemList() {
+        return itemList;
+    }
+
+    public void setItemList(List<SearchItem> itemList) {
+        this.itemList = itemList;
+    }
+
+    private int totlaPages;
+    private List<SearchItem> itemList;
+}
