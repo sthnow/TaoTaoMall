548ef670b49edb29e6e428f7b6053132fdf83d61
wangzh <568780720@qq.com>
2019-02-02 00:01:35 +0800

    添加商品同步索引库-messageListener

--- taotao-search/taotao-search-service/src/main/java/com/taotao/search/com/taotao/search/listener/itemAddMessageListener.java
+++ taotao-search/taotao-search-service/src/main/java/com/taotao/search/com/taotao/search/listener/itemAddMessageListener.java
@@ -1,21 +1,64 @@
 package com.taotao.search.com.taotao.search.listener;
 
+import com.taotao.common.pojo.SearchItem;
+import com.taotao.search.mapper.SearchItemMapper;
+import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.apache.solr.common.SolrDocument;
+import org.apache.solr.common.SolrInputDocument;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+import javax.swing.text.TabExpander;
+import java.io.IOException;
 
 /**
  * 监听商品添加事件，同步索引库
  */
 public class itemAddMessageListener implements MessageListener {
 
+    @Autowired
+    private SearchItemMapper searchItemMapper;
+    @Autowired
+    private SolrServer solrServer;
     @Override
     public void onMessage(Message message) {
         //从消息中取商品id
+        TextMessage textMessage = (TextMessage) message;
+        String text = null;
+        try {
+            text = textMessage.getText();
+            long itemId = Long.parseLong(text);
+            //等待事务提交
+            Thread.sleep(1000);
+            //根据商品id查询数据库，取商品信息
+            SearchItem searchItem = searchItemMapper.getItemById(itemId);
+            //创建文档对象
+            SolrInputDocument document = new SolrInputDocument();
+            //向文档对象中添加域
+            document.addField("id", searchItem.getId());
+            document.addField("item_title", searchItem.getTitle());
+            document.addField("item_sell_point", searchItem.getSell_point());
+            document.addField("item_price", searchItem.getPrice());
+            document.addField("item_image", searchItem.getImage());
+            document.addField("item_category_name", searchItem.getCategory_name());
+            document.addField("item_desc", searchItem.getItem_desc());
+            //把文档对象写入索引库
+            solrServer.add(document);
+            solrServer.commit();
+            //提交
+        } catch (JMSException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (SolrServerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        //根据商品id查询数据库，取商品信息
-        //创建文档对象
-        //向文档对象中添加域
-        //把文档对象写入索引库
-        //提交
     }
 }
