ced2f5bac19caeac8777193a0375d8fbba075bc6
wangzh <568780720@qq.com>
2019-02-02 13:15:37 +0800

    商品详情页面展示-测试成功

--- taotao-item-web/src/main/java/com/taotao/item/controller/ItemController.java
+++ taotao-item-web/src/main/java/com/taotao/item/controller/ItemController.java
@@ -5,6 +5,7 @@ import com.taotao.pojo.TbItem;
 import com.taotao.pojo.TbItemDesc;
 import com.taotao.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * 商品详情页面展示controller
  */
+@Controller
 public class ItemController {
 
     @Autowired
