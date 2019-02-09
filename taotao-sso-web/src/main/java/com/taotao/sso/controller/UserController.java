a963fc5d6fff74c708c0561bc05b20fd9bd6fd74
wangzh <568780720@qq.com>
2019-02-09 22:44:55 +0800

    用户注册-表现层

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.taotao.sso.controller;
 
 import com.taotao.common.pojo.TaotaoResult;
+import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -22,6 +24,12 @@ public class UserController {
     public TaotaoResult checkUserData(@PathVariable String param, @PathVariable Integer type){
         TaotaoResult result = userService.checkUserData(param, type);
         return result;
+    }
 
+    @RequestMapping(value = "/user/register",method = RequestMethod.POST)
+    @ResponseBody
+    public TaotaoResult register(TbUser user){
+        TaotaoResult result = userService.register(user);
+        return result;
     }
 }
