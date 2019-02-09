207746b690dfeede8800dd70af10ac891d7bde06
wangzh <568780720@qq.com>
2019-02-09 13:48:13 +0800

    用户祖册-service

--- taotao-sso/taotao-sso-service/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
+++ taotao-sso/taotao-sso-service/src/main/java/com/taotao/sso/service/impl/UserServiceImpl.java
@@ -5,9 +5,12 @@ import com.taotao.mapper.TbUserMapper;
 import com.taotao.pojo.TbUser;
 import com.taotao.pojo.TbUserExample;
 import com.taotao.sso.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -47,6 +50,53 @@ public class UserServiceImpl implements UserService {
         }
         //未查询到数据，数据可用
         return TaotaoResult.ok(true);
+
+    }
+
+    /**
+     * 注册用户
+     * @param user
+     * @return
+     */
+    @Override
+    public TaotaoResult register(TbUser user) {
+        //检查数据的有效性
+        if(StringUtils.isBlank(user.getUsername())){
+            return TaotaoResult.build(400, "用户名不能为空");
+        }
+        //判断用户名是否重复
+        TaotaoResult result = checkUserData(user.getUsername(), 1);
+        if (!(boolean) result.getData()) {
+            return TaotaoResult.build(400, "用户名重复");
+        }
+        //判断密码是否为空
+        if (StringUtils.isBlank(user.getPassword())) {
+            return TaotaoResult.build(400, "密码不能为空");
+        }
+        if (StringUtils.isNotBlank(user.getPhone())) {
+            //是否重复校验
+            TaotaoResult phoneResult = checkUserData(user.getPhone(), 2);
+            if(!(boolean)phoneResult.getData()){
+                return TaotaoResult.build(400, "电话号码重复");
+            }
+        }
+        //如果email不为空，进行重复性校验
+        if (StringUtils.isNotBlank(user.getEmail())) {
+            //是否重复校验
+            TaotaoResult phoneResult = checkUserData(user.getEmail(), 2);
+            if(!(boolean)phoneResult.getData()){
+                return TaotaoResult.build(400, "email重复");
+            }
+        }
+        //补全pojo属性
+        user.setCreated(new Date());
+        user.setUpdated(new Date());
+        //密码要md5加密
+        String md5Pass = DigestUtils.md5DigestAsHex(user.getPassword().getBytes());
+        user.setPassword(md5Pass);
+        tbUserMapper.insert(user);
+        //返回注册成功
+        return TaotaoResult.ok();
 
     }
 }
