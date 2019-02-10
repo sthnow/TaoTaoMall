51a4b67726c19f590d032778f35ed174761e5829
wangzh <568780720@qq.com>
2019-02-10 18:29:16 +0800

    jsonp服务端处理

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -2,10 +2,13 @@ package com.taotao.sso.controller;
 
 import com.taotao.common.pojo.TaotaoResult;
 import com.taotao.common.utils.CookieUtils;
+import com.taotao.common.utils.JsonUtils;
 import com.taotao.pojo.TbUser;
 import com.taotao.sso.service.UserService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,16 +48,25 @@ public class UserController {
     @ResponseBody
     public TaotaoResult login(String username, String password, HttpServletRequest request, HttpServletResponse response) {
         TaotaoResult result = userService.login(username, password);
-        //把token写入cookie
-        CookieUtils.setCookie(request,response,TOKEN_KEY,result.getData().toString());
+        //登录成功后写cookie
+        if(result.getStatus() == 200){
+            CookieUtils.setCookie(request,response,TOKEN_KEY,result.getData().toString());
+        }
         return result;
     }
 
-    @RequestMapping(value = "/token/{token}",method = RequestMethod.GET)
+    @RequestMapping(value = "/user/token/{token}",method = RequestMethod.GET,
+    //指定返回响应数据的content-type
+            produces = MediaType.APPLICATION_JSON_UTF8_VALUE
+            )
     @ResponseBody
-    public TaotaoResult getUserByToken(@PathVariable String token){
+    public String getUserByToken(@PathVariable String token, String callback){
         TaotaoResult result = userService.getUserByToken(token);
-        return result;
+        //判断是否为jsonp请求
+        if (StringUtils.isNotBlank(callback)) {
+            return callback + "(" + JsonUtils.objectToJson(result) + ");";
+        }
+        return JsonUtils.objectToJson(result);
     }
 
 
